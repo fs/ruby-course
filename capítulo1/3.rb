938890cad76dc3288eb2ab80ad163f171b4bbee8
pedryvo <pedryvo@gmail.com>
2017-02-04 21:53:57 -0300

    brincando com números

--- capítulo1/3.rb
+++ capítulo1/3.rb
@@ -4,6 +4,6 @@ puts "Agora coloque um desconto:"
 tax = gets
 tax2 = tax.to_f / 100.0
 valuetemp = value.to_f * tax2.to_f
-total = value.to_f - valuetemp.to_f
+total = value.to_f - valuetemp
 
 puts "O desconto em cima de R$#{value} é #{tax}%, então o valor total R$#{total}"
